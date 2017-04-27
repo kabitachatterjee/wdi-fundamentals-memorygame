b463282f0d5a5dd2956a6e08d804e36f4bc45748
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-04-26 18:49:36 -0700

    added score card and reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,14 +22,19 @@ var cards = [
             ];
 
 var cardsInPlay = [];
+var score = 0;
 function checkForMatch(){
 	if( cardsInPlay.length === 2 ){
 	if( cardsInPlay[0] === cardsInPlay[1] ){
-		alert("You found a match!");
-	}
+		score = score + 1;
+		alert("You found a match! Your score is " + score);
+		}
 	else {
-		alert("Sorry, try again");
+		alert("Sorry, try again. Your score is " + score);
+
 	}
+	var text = 'Your score is ' + score;
+		document.getElementById('score-board').innerHTML = text;
 }
 }
 
@@ -40,7 +45,6 @@ function flipCard(){
 	console.log(cards[cardId].image);
 	this.setAttribute('src',cards[cardId].image);
 	checkForMatch();
-
 }
 
 function createBoard(){
@@ -50,7 +54,22 @@ function createBoard(){
 		cardElement.setAttribute('data-id',i);
 		cardElement.addEventListener('click',flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
+		document.getElementById('reset').addEventListener('click',reset);
 	}
+}
+
+function reset(){
+	cardsInPlay = [];
+	var el = document.getElementsByTagName('img');
+	for( var i = 0; i < el.length; i++){
+		if(el[i].attributes.src !== 'images/back.png'){
+			el[i].setAttribute('src','images/back.png');
+		}
+		else {
+			console.log('hello');
+		}
+	}
+
 }
 
 createBoard();
